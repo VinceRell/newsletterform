ff15b4a3a81a29c6eb69a4a0d2e860b887494701
Vincent <v.rell_home@outlook.com>
2019-05-25 03:26:05 +0200

    finished newsletter form

--- app.js
+++ app.js
@@ -15,20 +15,50 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // signup route handler 
 app.post("/signup", (req, res) => {
-   const { firstname, lastname, email } = req.body;
+   const { firstName, lastName, email } = req.body;
 
 //    checking if fields are filled in 
-   if(!firstname || !lastname || !email) {
+   if(!firstName || !lastName || !email) {
        res.redirect("/fail.html");
        return;
    }
 
+   // Construct request data
+   const data = {
+       members: [
+           {
+               email_address: email,
+               staus: "subscribed",
+               merge_fields: {
+                   FNAME: firstName,
+                   LNAME: lastName
+               }
+           }
+       ]
+   };
+
+   //convert post-data object to string    
+   const postData = JSON.stringify(data);
+
    const options = {
-       url: ''
+       url: 'https://us20.api.mailchimp.com/3.0/lists/c756379f1a',
+       method: 'POST',
+       headers: {
+           Authorization: 'auth 01cb2db8a3a7a4449c6524a652ee30f8-us20'
+       },
+       body: postData
    }
 
    request(options, (err, response, body) => {
-
+        if(err) {
+            res.redirect("/fail.html");
+        } else {
+            if(response.statusCode === 200) {
+                res.redirect("/success.html");
+            } else {
+                res.redirect("/fail.html");
+            }
+        }
    });
 });
 
